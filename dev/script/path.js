d97ee1a206ecb553c1677c3310a99a240dadb2ed
DESKTOP-8IQMI96\Creasy <yklorse@gmail.com>
2017-08-11 17:30:39 +0800

    u

--- dev/script/path.js
+++ dev/script/path.js
@@ -1,7 +1,33 @@
+import $ from 'jquery';
+
 export default class {
     constructor(pointA, pointB) {
-        this.d = `M ${pointA.getX()} ${pointA.getY()}`;
+        this.pointStart = pointA;
+        this.pointEnd = pointB;
 
-        console.log(this.d);
+        this.render();
+    }
+    render() {
+        let d = `M ${this.pointStart.getX()} ${this.pointStart.getY()}`;
+
+        const gap = {
+            x: this.pointEnd.getX() - this.pointStart.getX(),
+            y: this.pointEnd.getY() - this.pointStart.getY(),
+        };
+
+        d = `${d} L ${this.pointStart.getX()} ${this.pointStart.getY() + gap.y / 2}`;
+
+        const $path = $(`
+            <path></path>
+        `);
+
+        $path
+            .attr('stroke', '#59ABE4')
+            .attr('stroke-width', 2)
+            .attr('fill', '#fff')
+            .attr('fill-opacity', 0)
+            .attr('d', $('.path-1').attr('d'));
+
+        $('svg').append($path);
     }
 }
