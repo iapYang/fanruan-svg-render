1c4eb8eb3a7fd680b70ca77ab1d357f0b52273ce
iapYang <yklorse@gmail.com>
2017-08-13 23:32:18 +0800

    u

--- dev/script/index.js
+++ dev/script/index.js
@@ -5,9 +5,57 @@ import Path from './path';
 const svg = document.getElementById('svg');
 
 new Path(svg, {
-    selector: '.red',
-    position: 'top',
+    selector: '.div1',
+    position: 'right',
 }, {
-    selector: '.blue',
+    selector: '.div2',
+    position: 'left',
+});
+
+new Path(svg, {
+    selector: '.div2',
+    position: 'bottom',
+}, {
+    selector: '.div3',
     position: 'top',
+});
+
+new Path(svg, {
+    selector: '.div3',
+    position: 'right',
+}, {
+    selector: '.div4',
+    position: 'left',
+});
+
+new Path(svg, {
+    selector: '.div4',
+    position: 'right',
+}, {
+    selector: '.div5',
+    position: 'left',
+});
+
+new Path(svg, {
+    selector: '.div5',
+    position: 'right',
+}, {
+    selector: '.div6',
+    position: 'left',
+});
+
+new Path(svg, {
+    selector: '.div8',
+    position: 'right',
+}, {
+    selector: '.div9',
+    position: 'left',
+});
+
+new Path(svg, {
+    selector: '.div9',
+    position: 'right',
+}, {
+    selector: '.div10',
+    position: 'left',
 });
