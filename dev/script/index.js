5973053c798c6392d589c1f64b0a864bb4094832
iapYang <yklorse@gmail.com>
2017-08-14 00:10:08 +0800

    u

--- dev/script/index.js
+++ dev/script/index.js
@@ -59,3 +59,91 @@ new Path(svg, {
     selector: '.div10',
     position: 'left',
 });
+
+new Path(svg, {
+    selector: '.div11',
+    position: 'right',
+}, {
+    selector: '.div12',
+    position: 'left',
+});
+
+new Path(svg, {
+    selector: '.div12',
+    position: 'right',
+}, {
+    selector: '.div13',
+    position: 'left',
+});
+
+new Path(svg, {
+    selector: '.div7',
+    position: 'right',
+}, {
+    selector: '.div11',
+    position: 'top',
+});
+
+new Path(svg, {
+    selector: '.div5',
+    position: 'bottom',
+}, {
+    selector: '.div14',
+    position: 'top',
+});
+
+new Path(svg, {
+    selector: '.div15',
+    position: 'right',
+}, {
+    selector: '.div16',
+    position: 'left',
+});
+
+new Path(svg, {
+    selector: '.div16',
+    position: 'right',
+}, {
+    selector: '.div17',
+    position: 'left',
+});
+
+new Path(svg, {
+    selector: '.div13',
+    position: 'bottom',
+}, {
+    selector: '.div16',
+    position: 'top',
+});
+
+new Path(svg, {
+    selector: '.div14',
+    position: 'bottom',
+}, {
+    selector: '.div18',
+    position: 'top',
+});
+
+new Path(svg, {
+    selector: '.div19',
+    position: 'right',
+}, {
+    selector: '.div20',
+    position: 'left',
+});
+
+new Path(svg, {
+    selector: '.div20',
+    position: 'right',
+}, {
+    selector: '.div21',
+    position: 'left',
+});
+
+new Path(svg, {
+    selector: '.div7',
+    position: 'bottom',
+}, {
+    selector: '.div19',
+    position: 'top',
+});
