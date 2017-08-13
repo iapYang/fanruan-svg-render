7700044ee01cce06abe3a4dd02b1e67e073f990d
iapYang <yklorse@gmail.com>
2017-08-13 21:13:27 +0800

    u

--- dev/script/path.js
+++ dev/script/path.js
@@ -1,15 +1,73 @@
 import $ from 'jquery';
 
 import Box from './box';
+import Line from './line';
+import Point from './point';
 
 export default class {
     constructor(svg, box1, box2) {
         this.svg = svg;
+        this.position1 = box1.position;
+        this.position2 = box2.position;
+
         this.box1 = new Box(box1.selector);
         this.box2 = new Box(box2.selector);
 
-        this.startPoint = this.box1[box1.position]();
-        this.endPoint = this.box2[box2.position]();
+        this.startPoint = this.box1[this.position1]();
+        this.endPoint = this.box2[this.position2]();
+
+        this.ssp = this.startPoint;
+        this.sep = this.endPoint;
+
+        this.render();
+    }
+    calcFinalPoint() {
+        this.startPoint = this.box1[this.position1]();
+        this.endPoint = this.box2[this.position2]();
+
+        this.strightLine = new Line(this.startPoint, this.endPoint);
+
+        if (this.box1.crossCount(this.strightLine) + this.box2.crossCount(this.strightLine) > 2) {
+            this.ssp = this.getFinalPoint(this.position1, this.startPoint);
+            this.sep = this.getFinalPoint(this.position2, this.endPoint);
+        } else {
+            this.ssp = this.startPoint;
+            this.sep = this.endPoint;
+        }
+    }
+    getFinalPoint(position, point) {
+        let x = 0, y = 0;
+        switch (position) {
+            case 'top':
+                x = point.getX();
+                y = point.getY() - 10;
+                break;
+            case 'left':
+                x = point.getX() - 10;
+                y = point.getY();
+                break;
+            case 'bottom':
+                x = point.getX();
+                y = point.getY() + 10;
+                break;
+            case 'right':
+                x = point.getX() + 10;
+                y = point.getY();
+                break;
+            default:
+                break;
+        }
+
+        return new Point({
+            x,
+            y,
+        });
+    }
+    refresh() {
+        this.startPoint = this.box1[this.position1]();
+        this.endPoint = this.box2[this.position2]();
+
+
 
         this.ssp = this.startPoint;
         this.sep = this.endPoint;
@@ -21,8 +79,8 @@ export default class {
         const ifLefter = this.ssp.getX() >= this.sep.getX();
 
         const gap = {
-            x: -(this.ssp.getX() - this.sep.getX()),
-            y: -(this.ssp.getY() - this.sep.getY()),
+            x: -this.ssp.getX() + this.sep.getX(),
+            y: -this.ssp.getY() + this.sep.getY(),
         };
 
         const d = `M ${this.ssp.getX()} ${this.ssp.getY()} l 0 ${gap.y / 2} l ${gap.x} 0 l 0 ${gap.y / 2}`;
